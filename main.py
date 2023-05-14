750d82556a313ecc95e7266c37a58136bd6626b7
Alvin Yeo <alvin.y@digipen.edu>
2023-05-14 22:59:57 +0800

    modified main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, request
 import numpy as np
 import pickle
-import sklearn
 
 app = Flask(__name__)
-# model = pickle.load(open('model.pkl', 'rb'))
-# scaler = pickle.load(open('scaler.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+scaler = pickle.load(open('scaler.pkl', 'rb'))
 
 # @app.route("/")
 # def index():
@@ -14,21 +13,21 @@ app = Flask(__name__)
 @app.route('/', methods=["GET"])
 @app.route('/index', methods=["GET"])
 def index():
-    return render_template('index2.html')
+    return render_template('index.html')
 
 
-# @app.route('/predict',methods=['POST'])
-# def predict():
-#     '''
-#     For rendering results on HTML GUI
-#     '''
-#     temp = list(request.form.values())
-#     int_features = [int(x) for x in temp[0].split(' ')]
-#     print(int_features)
-#     final_features = [np.array(int_features)]
-#     data = scaler.transform(final_features)
-#     prediction = model.predict(data)
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    temp = list(request.form.values())
+    int_features = [int(x) for x in temp[0].split(' ')]
+    print(int_features)
+    final_features = [np.array(int_features)]
+    data = scaler.transform(final_features)
+    prediction = model.predict(data)
 
-#     output = round(prediction[0], 2)
+    output = round(prediction[0], 2)
 
-#     return render_template('index.html', prediction_text='Predicted house price is {}'.format(output))
+    return render_template('index.html', prediction_text='Predicted house price is {}'.format(output))
